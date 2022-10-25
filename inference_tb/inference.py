79ef998bf147f5040acf881357d48da97b198272
vjbytes102 <vy386@nyu.edu>
2022-10-25 10:56:32 -0400

    prediction file attributes

--- inference_tb/inference.py
+++ inference_tb/inference.py
@@ -12,8 +12,8 @@ def prediction(input_path, model_path):
 	onnx_pred = model_inference.run(['output'], {"input": input_df})
 	pred = np.argmax(onnx_pred[0], axis=1)
 
-	df_pred = pd.DataFrame(input_df['StudyID'], columns=['Study_id']) #Paitent id
-	df_pred['Prediction'] = pred
+	df_pred = pd.DataFrame(input_df['StudyID'], columns=['participant']) #Paitent id
+	df_pred['probability'] = pred
 	df_pred.to_csv('/output/predictions.csv', index=False)
 
 if __name__ == '__main__':
