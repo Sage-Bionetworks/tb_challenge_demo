4e3ee3b5ecee228bac6c9804d4d5db252ba5d067
vjbytes102 <vy386@nyu.edu>
2022-11-02 03:37:34 -0400

    attribute update

--- inference_tb/inference.py
+++ inference_tb/inference.py
@@ -12,14 +12,14 @@ def prediction(input_path, model_path):
 	onnx_pred = model_inference.run(['output'], {"input": input_df})
 	pred = np.argmax(onnx_pred[0], axis=1)
 
-	df_pred = pd.DataFrame(input_df['StudyID'], columns=['participant']) #Paitent id
+	df_pred = pd.DataFrame(input_df['participant'], columns=['participant']) #Paitent id
 	df_pred['probability'] = pred
 	df_pred.to_csv('/output/predictions.csv', index=False)
 
 if __name__ == '__main__':
     
     #holdout sample file path 
-    input_path = '/input/*' # Utilize 'meta_info.csv' & 'raw_test_data/*.wav' according to your preprocessing logic
+    input_path = '/input/*' # Utilize 'meta_info.csv' , 'raw_test_data/*.wav' according to your preprocessing logic
     model_path = 'model/*.onnx' #model path
 
 	prediction(input_path, model_path)
